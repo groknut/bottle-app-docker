e4f9297636a29321f1b162b65474bc30ec174480
groknut <184639205+groknut@users.noreply.github.com>
2025-11-13 17:21:04 +0800

    create main.py script

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from bottle-app-docker!")
 
+from bottle import route, run, template, static_file
+from os import listdir
+from random import choice
+
+IMG_DIR = "./static/images/"
+
+@route('/')
+def main():    
+    img = choice(listdir(IMG_DIR))
+    return template('index.html', image=img)
+
+@route('/static/<filename:path>')
+def static(filename):
+    return static_file(filename, root='./static')
 
 if __name__ == "__main__":
-    main()
+    run(
+        host='localhost', port=5000, debug=True
+    )
